64ae459dcff984a440d3974a908930428af37911
Andy Venus <andy@avscripts.net>
2014-10-24 22:51:19 +0100

    Neatened Blog installer syntax

--- src/AVCMS/Bundles/Blog/Install/BlogInstaller.php
+++ src/AVCMS/Bundles/Blog/Install/BlogInstaller.php
@@ -20,20 +20,22 @@ class BlogInstaller extends BundleInstaller
 
     public function install_1_0_0()
     {
-        $this->PDO->exec("CREATE TABLE `{$this->prefix}blog_posts` (
-          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-          `title` text,
-          `body` text,
-          `user_id` varchar(200) DEFAULT NULL,
-          `testone__something` varchar(80) DEFAULT NULL,
-          `published` int(11) DEFAULT NULL,
-          `date_added` int(11) DEFAULT NULL,
-          `date_edited` int(11) DEFAULT NULL,
-          `creator_id` int(11) DEFAULT NULL,
-          `editor_id` int(11) DEFAULT NULL,
-          `slug` varchar(255) DEFAULT NULL,
-          `publish_date` int(11) DEFAULT NULL,
-          PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+        $this->PDO->exec("
+              CREATE TABLE `{$this->prefix}blog_posts` (
+                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                  `title` text,
+                  `body` text,
+                  `user_id` varchar(200) DEFAULT NULL,
+                  `testone__something` varchar(80) DEFAULT NULL,
+                  `published` int(11) DEFAULT NULL,
+                  `date_added` int(11) DEFAULT NULL,
+                  `date_edited` int(11) DEFAULT NULL,
+                  `creator_id` int(11) DEFAULT NULL,
+                  `editor_id` int(11) DEFAULT NULL,
+                  `slug` varchar(255) DEFAULT NULL,
+                  `publish_date` int(11) DEFAULT NULL,
+                  PRIMARY KEY (`id`)
+              ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ");
     }
 }
