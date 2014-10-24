f5b9175517e5239266b9612d2594530d09531a6b
Andy Venus <andy@avscripts.net>
2014-10-24 15:29:05 +0100

    Added InstallerServices

---
+++ src/AVCMS/Bundles/Installer/Services/InstallerServices.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Andy
+ * Date: 24/10/14
+ * Time: 15:25
+ */
+
+namespace AVCMS\Bundles\Installer\Services;
+
+use AV\Service\Service;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class InstallerServices implements Service
+{
+    public function getServices($configuration, ContainerBuilder $container)
+    {
+        // TODO: Implement getServices() method.
+    }
+}
