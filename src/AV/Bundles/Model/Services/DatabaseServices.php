0babb174133bd093e153c221ad15947105567f9b
Andy Venus <andy@avscripts.net>
2014-11-24 11:39:51 +0000

    Made database config location configurable

--- src/AV/Bundles/Model/Services/DatabaseServices.php
+++ src/AV/Bundles/Model/Services/DatabaseServices.php
@@ -18,7 +18,7 @@ class DatabaseServices implements ServicesInterface
     public function getServices($configuration, ContainerBuilder $container)
     {
         $container->register('query_builder.factory', 'AV\Model\Connection')
-            ->setArguments(array('mysql', '%config_dir%/database.php', null, null, new Reference('dispatcher')));
+            ->setArguments(array('mysql', '%database_config_location%', null, null, new Reference('dispatcher')));
 
         $container->setDefinition('query_builder', new Definition('AV\Model\QueryBuilder\QueryBuilderHandler'))
             ->setFactoryService('query_builder.factory')
