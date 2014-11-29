c231d6af2c42437a7f225ec14e4f229e74166802
Andy Venus <andy@avscripts.net>
2014-11-29 12:20:13 +0000

    Fixed UserActivitySubscriber's TokenStorageInterface use statement

--- src/AVCMS/Bundles/Users/Subscriber/UserActivitySubscriber.php
+++ src/AVCMS/Bundles/Users/Subscriber/UserActivitySubscriber.php
@@ -11,8 +11,7 @@ use AVCMS\Bundles\Users\Model\Users;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Security\Core\SecurityContextInterface;
-use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserActivitySubscriber implements EventSubscriberInterface
 {
