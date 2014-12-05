975e390a9b6ca1658bee73e85b6c74e80c7c3d9d
Andy Venus <andy@avscripts.net>
2014-12-05 12:18:31 +0000

    Make asset regen a page for manual regeneration

--- src/AVCMS/Bundles/Assets/Controller/AssetsController.php
+++ src/AVCMS/Bundles/Assets/Controller/AssetsController.php
@@ -8,22 +8,41 @@
 namespace AVCMS\Bundles\Assets\Controller;
 
 use Assetic\AssetWriter;
+use AVCMS\Bundles\Admin\Controller\AdminBaseController;
 use AVCMS\Core\AssetManager\Asset\BundleAssetInterface;
 use AVCMS\Core\AssetManager\Asset\TemplateAssetInterface;
 use AVCMS\Core\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class AssetsController extends Controller
+class AssetsController extends AdminBaseController
 {
-    public function generateAssetsAction()
+    public function generateAssetsAction(Request $request)
     {
         $fileMover = $this->get('public_file_mover');
         $fileMover->doMove(true);
 
         $assetManager = $this->get('asset_manager');
-        $assetManager->generateProductionAssets(new AssetWriter('web/compiled'));
+        $error = null;
 
-        return new Response('Assets generated');
+        try {
+            $assetManager->generateProductionAssets(new AssetWriter('web/compiled'));
+        }
+        catch (\Exception $e) {
+            $error = $e->getMessage();
+        }
+
+        if ($request->get('ajax_depth') !== null || !$request->isXmlHttpRequest()) {
+            return new Response($this->renderAdminSection(
+                    '@Assets/asset_regen.twig',
+                    $request->get('ajax_depth'),
+                    array('error' => $error)
+                )
+            );
+        }
+        else {
+            return new JsonResponse(['success' => is_null($error), 'error' => $error]);
+        }
     }
 }
