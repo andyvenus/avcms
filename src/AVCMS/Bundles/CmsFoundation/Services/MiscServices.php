1bb3b7ee33c71bf2808fdd8e9556600bb7927527
Andy Venus <andy@avscripts.net>
2014-12-10 16:13:24 +0000

    Allow webmaster to override URL's

--- src/AVCMS/Bundles/CmsFoundation/Services/MiscServices.php
+++ src/AVCMS/Bundles/CmsFoundation/Services/MiscServices.php
@@ -16,6 +16,10 @@ class MiscServices implements ServicesInterface
 {
     public function getServices($configuration, ContainerBuilder $container)
     {
+        $container->register('router', 'AVCMS\Core\Kernel\Router')
+            ->setArguments(array(new Reference('router.loader.yaml'), 'routes.yml', '%app_dir%', '%root_dir%', array('cache_dir' => '%cache_dir%', 'debug' => '%dev_mode%'), new Reference('bundle_manager')))
+        ;
+
         $container->register('site_offline_handler', 'AVCMS\Core\Security\SiteOfflineHandler')
             ->setArguments([new Reference('auth.access_denied_handler')])
             ->addTag('event.listener', ['event' => KernelEvents::EXCEPTION, 'method' => 'onKernelException', 'priority' => 100])
---
+++ src/AVCMS/Core/Kernel/Router.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: Andy
+ * Date: 10/12/14
+ * Time: 15:29
+ */
+
+namespace AVCMS\Core\Kernel;
+
+use AV\Kernel\Bundle\BundleManagerInterface;
+use AV\Kernel\Router as BaseRouter;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Routing\RequestContext;
+
+class Router extends BaseRouter
+{
+    protected $rootDir;
+
+    public function __construct(LoaderInterface $loader, $resource, $appDir, $rootDir, array $options = array(), BundleManagerInterface $bundleManager = null, RequestContext $context = null, LoggerInterface $logger = null)
+    {
+        $this->rootDir = $rootDir;
+        parent::__construct($loader, $resource, $appDir, $options, $bundleManager, $context, $logger);
+    }
+
+    public function getRouteCollection()
+    {
+        if (null === $this->collection) {
+            parent::getRouteCollection();
+
+            if (file_exists($this->rootDir.'/webmaster/routes.yml')) {
+                $webmasterRoutes = $this->loader->load($this->rootDir.'/webmaster/routes.yml', $this->options['resource_type']);
+                $this->collection->addCollection($webmasterRoutes);
+            }
+        }
+
+        return $this->collection;
+    }
+} 
