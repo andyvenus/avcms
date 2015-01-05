c68158303b4367187acd244fdf5ad1bb275c6eaa
Andy Venus <andy@avscripts.net>
2015-01-05 18:29:06 +0000

    Add separate route for wallpaper categories

--- src/AVCMS/Bundles/Wallpapers/Services/WallpaperServices.php
+++ src/AVCMS/Bundles/Wallpapers/Services/WallpaperServices.php
@@ -30,7 +30,7 @@ class WallpaperServices implements ServicesInterface
         ;
 
         $container->register('menu_types.wallpaper_categories', 'AVCMS\Bundles\Categories\MenuItemType\CategoriesMenuItemType')
-            ->setArguments([new Reference('wallpaper.categories_model'), new Reference('router'), 'browse_wallpapers'])
+            ->setArguments([new Reference('wallpaper.categories_model'), new Reference('router'), 'wallpaper_category'])
             ->addMethodCall('setName', ['Wallpaper Categories'])
             ->addTag('menu.item_type', ['id' => 'wallpaper_categories'])
         ;
