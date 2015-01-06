73a891dfaf741d55c17f1ff76dba9822b1e9824c
Andy Venus <andy@avscripts.net>
2015-01-06 16:09:20 +0000

    Add basic liked wallpapers modules for profiles

--- src/AVCMS/Bundles/Wallpapers/Controller/WallpaperModulesController.php
+++ src/AVCMS/Bundles/Wallpapers/Controller/WallpaperModulesController.php
@@ -7,6 +7,7 @@
 
 namespace AVCMS\Bundles\Wallpapers\Controller;
 
+use AVCMS\Bundles\Users\Model\User;
 use AVCMS\Core\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -30,6 +31,28 @@ class WallpaperModulesController extends Controller
             ->published()
             ->join($this->model('WallpaperCategories'), ['id', 'name', 'slug'])
             ->get();
+
+        return new Response($this->render('@Wallpapers/wallpapers_list_module.twig', array('wallpapers' => $wallpapers, 'user_settings' => $userSettings)));
+    }
+
+    public function likedWallpapersModule($userSettings, User $user)
+    {
+        $ratings = $this->model('AVCMS\Bundles\LikeDislike\Model\Ratings');
+
+        $liked = $ratings->query()
+            ->where('user_id', $user->getId())
+            ->where('content_type', 'wallpaper')
+            ->where('rating', 1)
+            ->select(['content_id'])
+            ->limit($userSettings['limit'])
+        ->get();
+
+        $ids = [];
+        foreach ($liked as $like) {
+            $ids[] = $like->getContentId();
+        }
+
+        $wallpapers = $this->wallpapers->query()->whereIn('id', $ids)->get();
 
         return new Response($this->render('@Wallpapers/wallpapers_list_module.twig', array('wallpapers' => $wallpapers, 'user_settings' => $userSettings)));
     }
