6e4ebd442f026d7483872e5cc0bb8e428a29bb3b
Andy Venus <andy@avscripts.net>
2015-01-09 11:26:53 +0000

    Add meta descriptions to wallpaper categories

--- src/AVCMS/Bundles/Wallpapers/Model/WallpaperCategory.php
+++ src/AVCMS/Bundles/Wallpapers/Model/WallpaperCategory.php
@@ -6,5 +6,13 @@ use AVCMS\Bundles\Categories\Model\Category;
 
 class WallpaperCategory extends Category
 {
+    public function setDescription($value)
+    {
+        $this->set('description', $value);
+    }
 
+    public function getDescription()
+    {
+        return $this->get('description');
+    }
 }
