a293a5dd09fb488b591dc2cb10e0184034a73ee2
Andy Venus <andy@avscripts.net>
2015-01-19 12:15:01 +0000

    Add 'exact value' validation rule

---
+++ src/AV/Validation/Rules/ExactValue.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Andy
+ * Date: 19/01/15
+ * Time: 12:09
+ */
+
+namespace AV\Validation\Rules;
+
+class ExactValue extends Rule
+{
+    protected $value;
+
+    protected $error = 'The field {param_name} must be exactly {required_value}';
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    function assert($param)
+    {
+        if ($param !== $this->value) {
+            return false;
+        }
+
+        return true;
+    }
+}
