369b7cd8f46747f746024338228b1b997e6a9cc1
Andy Venus <andy@avscripts.net>
2015-01-19 12:14:18 +0000

    Add width/height constraints to SymfonyImageFile validation rule

--- src/AV/Validation/Rules/SymfonyImageFile.php
+++ src/AV/Validation/Rules/SymfonyImageFile.php
@@ -11,15 +11,26 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class SymfonyImageFile extends Rule
 {
+    const MAX_SIZE = 'MAX';
+
+    const MIN_SIZE = 'MIN';
+
     protected $error = 'The file provided for {param_name} is not a valid image file';
 
-    public function __construct($width = null, $height = null)
+    protected $width;
+
+    protected $height;
+
+    protected $constraint;
+
+    public function __construct($width = null, $height = null, $constraint = self::MAX_SIZE)
     {
         $this->width = $width;
         $this->height = $height;
+        $this->constraint = $constraint;
     }
 
-    function assert($param)
+    public function assert($param)
     {
         if (!$param instanceof UploadedFile) {
             return false;
@@ -28,6 +39,17 @@ class SymfonyImageFile extends Rule
         $imageSize = getimagesize($param->getPathname());
         if (!$imageSize[0]) {
             return false;
+        }
+
+        if ($this->width !== null && $this->height !== null) {
+            if ($this->constraint === self::MAX_SIZE && ($imageSize[0] > $this->width || $imageSize[1] > $this->height)) {
+                $this->setError('The uploaded image cannot be a larger resolution than {width}x{height}', ['width' => $this->width, 'height' => $this->height]);
+                return false;
+            }
+            elseif ($this->constraint === self::MIN_SIZE && ($imageSize[0] < $this->width || $imageSize[1] < $this->height)) {
+                $this->setError('The uploaded image cannot be a smaller resolution than {width}x{height}', ['width' => $this->width, 'height' => $this->height]);
+                return false;
+            }
         }
 
         return true;
