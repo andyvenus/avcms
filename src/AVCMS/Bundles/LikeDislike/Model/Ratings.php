6c1f0da8bf570ff96f0f6d7e9c38d178f3a34ff5
Andy Venus <andy@avscripts.net>
2015-02-12 16:24:13 +0000

    Add getLikedIds to ratings model

--- src/AVCMS/Bundles/LikeDislike/Model/Ratings.php
+++ src/AVCMS/Bundles/LikeDislike/Model/Ratings.php
@@ -20,4 +20,25 @@ class Ratings extends Model
     {
         return 'AVCMS\Bundles\LikeDislike\Model\Rating';
     }
+
+    public function getLikedIds($userId, $contentType, $limit = null)
+    {
+        $query = $this->query()
+            ->where('user_id', $userId)
+            ->where('content_type', $contentType)
+            ->where('rating', 1)
+            ->select(['content_id'])
+            ->limit($limit)
+        ;
+
+        /** @var $results Rating[] */
+        $results = $query->get();
+
+        $ids = [];
+        foreach ($results as $like) {
+            $ids[] = $like->getContentId();
+        }
+
+        return $ids;
+    }
 }
