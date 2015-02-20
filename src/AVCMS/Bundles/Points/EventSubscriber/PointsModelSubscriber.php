a8d7e89ecb1532ca449735a98e04d314f0a57ffe
Andy Venus <andy@avscripts.net>
2015-02-20 16:45:32 +0000

    Make sure points are retrieved when a user is joined to another table

--- src/AVCMS/Bundles/Points/EventSubscriber/PointsModelSubscriber.php
+++ src/AVCMS/Bundles/Points/EventSubscriber/PointsModelSubscriber.php
@@ -8,6 +8,7 @@
 namespace AVCMS\Bundles\Points\EventSubscriber;
 
 use AV\Model\Event\CreateModelEvent;
+use AV\Model\Event\QueryBuilderModelJoinEvent;
 use AVCMS\Bundles\Users\Model\Users;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -24,10 +25,22 @@ class PointsModelSubscriber implements EventSubscriberInterface
         $model->addOverflowEntity('points', 'AVCMS\Bundles\Points\Model\PointsOverflow');
     }
 
+    public function addJoinColumn(QueryBuilderModelJoinEvent $event)
+    {
+        if (!$event->getJoinModel() instanceof Users) {
+            return;
+        }
+
+        $columns = $event->getColumns();
+        $columns[] = 'points__points';
+        $event->setColumns($columns);
+    }
+
     public static function getSubscribedEvents()
     {
         return [
-            'model.create' => ['addOverflowEntity']
+            'model.create' => ['addOverflowEntity'],
+            'query_builder.model_join' => ['addJoinColumn']
         ];
     }
 }
