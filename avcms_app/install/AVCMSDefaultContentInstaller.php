de5c15869a6f7001e7cb383e3b0855bf955879f0
Andy Venus <andy@avscripts.net>
2015-03-06 13:22:59 +0000

    Update default blog post

--- avcms_app/install/AVCMSDefaultContentInstaller.php
+++ avcms_app/install/AVCMSDefaultContentInstaller.php
@@ -241,8 +241,7 @@ class AVCMSDefaultContentInstaller extends \AVCMS\Core\Installer\DefaultContentI
         $post = $blogPosts->newEntity();
         $post->setTitle('Welcome to AVCMS');
         $post->setBody('<p>Welcome to AVCMS, a new modular content management system.</p>
-            <p>This first version is just a simple blog to test out the system and get it running well for the upcoming major releases from AV Scripts. Check out the custom menus and modules, or the powerful new admin panel and please send any feedback you have.</p>
-            <p>Andy</p>');
+            <p>This is just a first blog post to show the blog system. You can edit or delete it from the admin panel.</p>');
         $post->setSlug('welcome-to-avcms');
         $post->setPublishDate(time());
 
