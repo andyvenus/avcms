69871e96035d8f3b8ee49ba2b101096f0aca1ca8
Andy Venus <andy@avscripts.net>
2015-03-23 16:54:23 +0000

    Make whoops listener ignore 404s

--- src/AVCMS/BundlesDev/DevTools/Listener/WhoopsListener.php
+++ src/AVCMS/BundlesDev/DevTools/Listener/WhoopsListener.php
@@ -9,11 +9,16 @@ namespace AVCMS\BundlesDev\DevTools\Listener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class WhoopsListener
 {
     public function onTerminate(GetResponseForExceptionEvent $event)
     {
+        if ($event->getException() instanceof NotFoundHttpException) {
+            return;
+        }
+
         $whoops = new \Whoops\Run;
         $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
         $whoops->register();
