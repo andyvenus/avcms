2782b898788c1cacd8df09d25ceed96c8d58ec07
Andy Venus <andy@avscripts.net>
2015-04-11 20:26:13 +0100

    Add webmaster route_overrides.yml optional routes file

--- src/AVCMS/Core/Kernel/Router.php
+++ src/AVCMS/Core/Kernel/Router.php
@@ -11,6 +11,7 @@ use AV\Kernel\Bundle\BundleManagerInterface;
 use AV\Kernel\Router as BaseRouter;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\Routing\RequestContext;
 
 class Router extends BaseRouter
@@ -26,7 +27,19 @@ class Router extends BaseRouter
     public function getRouteCollection()
     {
         if (null === $this->collection) {
-            parent::getRouteCollection();
+            $this->collection = $this->loader->load($this->resource, $this->options['resource_type']);
+
+            if (file_exists($this->rootDir.'/webmaster/config/route_overrides.yml')) {
+                $webmasterOverrideRoutes = $this->loader->load($this->rootDir.'/webmaster/config/route_overrides.yml', $this->options['resource_type']);
+                $this->collection->addCollection($webmasterOverrideRoutes);
+            }
+
+            if ($this->bundleManager) {
+                $this->bundleManager->getBundleRoutes($this->collection);
+            }
+
+            $this->collection->addResource(new FileResource($this->appDir.'/config/bundles.yml'));
+            $this->collection->addResource(new FileResource($this->appDir.'/config/bundles_dev.yml'));
 
             if (file_exists($this->rootDir.'/webmaster/config/routes.yml')) {
                 $webmasterRoutes = $this->loader->load($this->rootDir.'/webmaster/config/routes.yml', $this->options['resource_type']);
