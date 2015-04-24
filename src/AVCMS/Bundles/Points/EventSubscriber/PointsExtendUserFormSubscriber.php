8405c8a07c5499bbdc4a98ff43d2c24bf546d007
Andy Venus <andy@avscripts.net>
2015-04-24 17:17:10 +0100

    Points user admin form, handle case-sensitive rule declaration

--- src/AVCMS/Bundles/Points/EventSubscriber/PointsExtendUserFormSubscriber.php
+++ src/AVCMS/Bundles/Points/EventSubscriber/PointsExtendUserFormSubscriber.php
@@ -24,7 +24,7 @@ class PointsExtendUserFormSubscriber implements EventSubscriberInterface
         $form->add('points', 'text', [
             'label' => 'Points',
             'validation' => [
-                ['rule' => 'numeric']
+                ['rule' => 'Numeric']
             ]
         ]);
     }
