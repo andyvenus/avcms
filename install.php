a9ec79509f9072e4654b207c0055e5553c69d74b
Andy Venus <andy@avscripts.net>
2015-04-29 16:49:10 +0100

    Fixed issue with the installer picking up webmaster-set bundles

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 
 if (file_exists('webmaster/installer_lock.txt')) exit('The installer is locked. To unlock, delete the webmaster/installer_lock.txt file');
 
-use AVCMS\Core\Kernel\AvcmsKernel;
+use AV\Kernel\BundleKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 define('DEBUG_MODE', true);
@@ -10,7 +10,7 @@ define('ROOT_DIR', __DIR__);
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$avcms = new AvcmsKernel(ROOT_DIR, DEBUG_MODE, ['app_dir' => 'avcms_app/install', 'cache_dir' => 'cache/installer']);
+$avcms = new BundleKernel(ROOT_DIR, DEBUG_MODE, ['app_dir' => 'avcms_app/install', 'cache_dir' => 'cache/installer']);
 
 $request = Request::createFromGlobals();
 $response = $avcms->handle($request);
