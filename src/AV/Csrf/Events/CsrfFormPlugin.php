f53a3e268a24b391e54f1c79bc7c5de15eda52e2
Andy Venus <andy@avscripts.net>
2015-05-14 12:38:14 +0100

    Better CSRF token error

--- src/AV/Csrf/Events/CsrfFormPlugin.php
+++ src/AV/Csrf/Events/CsrfFormPlugin.php
@@ -56,7 +56,7 @@ class CsrfFormPlugin implements EventSubscriberInterface
 
         if ($this->token->checkToken($token) === false) {
             $formHandler = $event->getFormHandler();
-            $formHandler->addCustomErrors(array(new FormError('_csrf_token', 'Invalid CSRF token. Got: "'.$token.'"" Expected: "'.$this->token->getToken().'""')));
+            $formHandler->addCustomErrors(array(new FormError('_csrf_token', 'Invalid CSRF token. Try refreshing the page and trying again.')));
         }
     }
 
