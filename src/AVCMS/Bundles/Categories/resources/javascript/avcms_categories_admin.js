65b5a063974e44baa8538437f83c358b35ba2374
Andy Venus <andy@avscripts.net>
2015-06-04 14:20:22 +0100

    Update category select fields when adding or editing categories

--- src/AVCMS/Bundles/Categories/resources/javascript/avcms_categories_admin.js
+++ src/AVCMS/Bundles/Categories/resources/javascript/avcms_categories_admin.js
@@ -5,9 +5,21 @@ $(document).ready(function() {
     $('body').on('click', '[data-reorder-categories-url]', avcms.categories.alphabeticallyReorderCategories);
 
     avcms.event.addEvent('submit-form-success', avcms.categories.onCategoryDeleted);
+
+    avcms.event.addEvent('submit-form', function(form) {
+        if (!form.data('edit-category')) {
+            return;
+        }
+
+        avcms.categories.edit_name = form.find('[name=name]').val();
+    });
+
+    avcms.event.addEvent('submit-form-success', avcms.categories.refreshCategorySelects);
 });
 
 avcms.categories = {
+    edit_name: null,
+
     openDeleteCategoryForm: function() {
         var button = $(this);
 
@@ -27,6 +39,24 @@ avcms.categories = {
             $.post($(this).data('reorder-categories-url'), function() {
                 avcms.nav.refreshSection('.ajax-editor-inner', 'editor');
             });
+        }
+    },
+
+    refreshCategorySelects: function(form, submit_data) {
+        if (!form.data('edit-category')) {
+            return;
+        }
+
+        var selects = $('select[name=category_id]');
+        var category_name = avcms.categories.edit_name;
+
+        var existing = selects.find('option[value='+submit_data.id+']');
+        if (existing.length) {
+            existing.text(category_name);
+        }
+        else {
+            var option = '<option value="' + submit_data.id + '">' + category_name + '</option>';
+            selects.append(option);
         }
     }
 };
