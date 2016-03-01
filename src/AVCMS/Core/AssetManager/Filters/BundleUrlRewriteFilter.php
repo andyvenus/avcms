edc10959bb8f200d42cc9b0c205d201ee9576bb6
Andy Venus <andy@eyfs.info>
2016-03-01 11:41:04 +0000

    Rewrite template asset URL's in CSS

--- src/AVCMS/Core/AssetManager/Filters/BundleUrlRewriteFilter.php
+++ src/AVCMS/Core/AssetManager/Filters/BundleUrlRewriteFilter.php
@@ -5,6 +5,7 @@ namespace AVCMS\Core\AssetManager\Filters;
 use Assetic\Asset\AssetInterface;
 use Assetic\Filter\BaseCssFilter;
 use AVCMS\Core\AssetManager\Asset\BundleFileAsset;
+use AVCMS\Core\AssetManager\Asset\TemplateFileAsset;
 
 class BundleUrlRewriteFilter extends BaseCssFilter
 {
@@ -14,22 +15,37 @@ class BundleUrlRewriteFilter extends BaseCssFilter
 
     public function filterDump(AssetInterface $asset)
     {
-        if (!($asset instanceof BundleFileAsset)) {
-            return;
+        if ($asset instanceof BundleFileAsset) {
+            $content = $this->filterReferences($asset->getContent(), function ($matches) use ($asset) {
+                if (false !== strpos($matches['url'], '://') || 0 === strpos($matches['url'], '//') || 0 === strpos($matches['url'], 'data:')) {
+                    return $matches[0];
+                }
+
+                if (isset($matches['url'][0]) && '/' == $matches['url'][0]) {
+                    return $matches[0];
+                }
+
+                return str_replace($matches['url'], '../resources/' . $asset->getBundle() . '/' . $asset->getType() . '/' . $matches['url'], $matches[0]);
+            });
+
+            $asset->setContent($content);
+        } elseif ($asset instanceof TemplateFileAsset) {
+            $content = $this->filterReferences($asset->getContent(), function ($matches) use ($asset) {
+                if (false !== strpos($matches['url'], '://') || 0 === strpos($matches['url'], '//') || 0 === strpos($matches['url'], 'data:')) {
+                    return $matches[0];
+                }
+
+                if (isset($matches['url'][0]) && '/' == $matches['url'][0]) {
+                    return $matches[0];
+                }
+
+                $fullPath = explode('/', $asset->getTemplate());
+                $templateName = array_pop($fullPath);
+
+                return str_replace($matches['url'], '../resources/templates/frontend/' . $templateName . '/' . $asset->getType() . '/' . $matches['url'], $matches[0]);
+            });
+
+            $asset->setContent($content);
         }
-
-        $content = $this->filterReferences($asset->getContent(), function($matches) use ($asset) {
-            if (false !== strpos($matches['url'], '://') || 0 === strpos($matches['url'], '//') || 0 === strpos($matches['url'], 'data:')) {
-                return $matches[0];
-            }
-
-            if (isset($matches['url'][0]) && '/' == $matches['url'][0]) {
-                return $matches[0];
-            }
-
-            return str_replace($matches['url'], '../resources/'.$asset->getBundle().'/'.$asset->getType().'/'.$matches['url'], $matches[0]);
-        });
-
-        $asset->setContent($content);
     }
 }
